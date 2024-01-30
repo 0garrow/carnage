0be11652508286d4406afc9ad36b12e615a12367
0garrow <0garrow@protonmail.com>
2024-01-30 22:11:34 +0000

    Add return to read top100.csv

--- attribs.py
+++ attribs.py
@@ -2,6 +2,7 @@ from random import randint
 from variables import attributes, attributes_nsfw
 import pandas as pd
 
+
 def get_attr(user_input, usermention, channel, username):
     # Convert discord module objects to strings.
     user_input = str(user_input)
@@ -13,23 +14,19 @@ def get_attr(user_input, usermention, channel, username):
     attr_nsfw_list = ", ".join(attributes_nsfw)
 
 
-
 def write_top100(username, user_input):
     with open("top100.csv", "a") as file:
         file.writelines(username, user_input)
 
-
-
-
     if user_input == "help" and channel.startswith("nsfw"):
-                return f"""# __Carnage Commands__ (attr)
+        return f"""# __Carnage Commands__ (attr)
         Here is the full list of currently available attributes **({len(attributes)})**:
         {attr_list}
 
         NSFW attributes 😈 **({len(attributes_nsfw)})**:
         {attr_nsfw_list}
         """
-    elif user_input == "help" :
+    elif user_input == "help":
         attributes.sort()
         attributes_nsfw.sort()
         return f"""# __Carnage Commands__ (attr)
@@ -43,20 +40,20 @@ def write_top100(username, user_input):
             top = f"{top}\n{index + 1}. {row}"
         return top
 
-
-    if channel.startswith("nsfw") and user_input in attributes or user_input in attr_nsfw_list:
+    if (
+        channel.startswith("nsfw")
+        and user_input in attributes
+        or user_input in attr_nsfw_list
+    ):
         return f"{usermention} is {randint(0, 100)}% {user_input} 😈"
     elif user_input in attributes:
         percentage = randint(0, 100)
         if username == "veno.m":
             percentage = 100
         if percentage == 100:
+            write_top100(username, user_input)
             return f"# Congratulations!!!\n{usermention} is {percentage}% {user_input} and has been added to our TOP 100 hall of fame"
         else:
             return f"{usermention} is {percentage}% {user_input}"
     else:
         return f"{usermention} '{user_input}' is not a valid attribute. To view a current list of valid attributes ,type `,attr help`"
-
-
-
-
