988eaafebbecce249890e026f690221c50d8a786
0garrow <0garrow@protonmail.com>
2024-01-30 20:41:15 +0000

    Move attributes in to variables file

--- attribs.py
+++ attribs.py
@@ -1,17 +1,5 @@
 from random import randint
-
-attributes =  ['happy', 'bastard', 'mischievous', 'bashful', 'cute', 
-'sexy', 'bitch', 'devil', 'enviable', 'sarcastic', "saddo", "crazy",
-'beautiful', 'sleepy', 'grumpy', 'horny', 'curious', 'positive', 
-'cynical', 'boring', 'vain', 'stupid', 'greedy', 'passionate', 
-'evil', 'dopey', 'caring', 'arrogant', 'rude', 'lazy', 'flirt',
-'outgoing', 'cool', 'daring', 'engaging', 'honest', 'cheap', 
-'fussy', 'jealous', 'perfectionist', 'smug', 'reliable', 'devilish'
-'witty', 'funny', 'sad', 'angry', 'enamoured', 'angel', 'nurturing']
-
-attributes_nsfw = ['kinky', 'sub', 'dom', 'switch', 'tabboo', 
-'slut', 'spankable', 'daddy', 'boss', 'mistress', 'sir', 'master', 
-'tied', 'stimulated', 'orgasmic', 'sensual']
+from variables import attributes, attributes_nsfw
 
 def get_attr(user_input, usermention, channel):
     user_input = str(user_input)
---
+++ variables.py
@@ -0,0 +1,12 @@
+attributes =  ['happy', 'bastard', 'mischievous', 'bashful', 'cute', 
+'sexy', 'bitch', 'devil', 'enviable', 'sarcastic', "saddo", "crazy",
+'beautiful', 'sleepy', 'grumpy', 'horny', 'curious', 'positive', 
+'cynical', 'boring', 'vain', 'stupid', 'greedy', 'passionate', 
+'evil', 'dopey', 'caring', 'arrogant', 'rude', 'lazy', 'flirt',
+'outgoing', 'cool', 'daring', 'engaging', 'honest', 'cheap', 
+'fussy', 'jealous', 'perfectionist', 'smug', 'reliable', 'devilish',
+'witty', 'funny', 'sad', 'angry', 'enamoured', 'angel', 'nurturing']
+
+attributes_nsfw = ['kinky', 'sub', 'dom', 'switch', 'tabboo', 
+'slut', 'spankable', 'daddy', 'boss', 'mistress', 'sir', 'master', 
+'tied', 'stimulated', 'orgasmic', 'sensual']
