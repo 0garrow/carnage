ee16d01ea220e9f0c4557f92e580640b0c3bd6b4
0garrow <0garrow@protonmail.com>
2024-01-30 21:04:11 +0000

    Move insults in to variables file

--- responses.py
+++ responses.py
@@ -1,6 +1,6 @@
 from random import randint, choice
 from attribs import get_attr
-import webbrowser
+from variables import insults
 
 MY_ID = "@1061655101015588885"
 PREFIX = ","
@@ -16,43 +16,7 @@ def get_response(user_input, username, usermention, channel):
     # elif lowered.startswith(PREFIX + "google"):
     #     webbrowser.open("https://www.google.com/search?q=" + user_input[7:])
     elif lowered.startswith(PREFIX + "insult"):
-        return choice(
-            [
-                f"{usermention} The closest you'll come to a brainstorm is a light drizzle.",
-                f"{usermention} Honestly, I'm just impressed you could read this.",
-                f"{usermention} Your family tree didn't have enough branches.",
-                f"{usermention} Your face just made me agnostic.",
-                f"{usermention} I'm still deciding whether you're the weakest link or the missing link.",
-                f"{usermention} I have neither the time nor the crayons necessary to explain this to you.",
-                f"{usermention} You are an oxygen bandit.",
-                f"{usermention} I've had bowel movements more attractive than you.",
-                f'{usermention} You look like a "before" picture.',
-                f"{usermention} You could have been an extra in Chernobyl.",
-                f"{usermention} What doesn't kill you disappoints the rest of us.",
-                f"{usermention} Did you develop your personality in car a crash?",
-                f"{usermention} I smell smoke. Were you thinking too hard again?",
-                f"{usermention} Being bitter won't make you prettier.",
-                f"{usermention} Out of all the sperm to win the race...",
-                f"{usermention} You'll go far someday. And I hope you stay there.",
-                f"{usermention} Your gene pool needs more chlorine.",
-                f"{usermention} I will not have a battle of wits with an unarmed ,opponent.",
-                f"{usermention} It's impossible to underestimate you.",
-                f"{usermention} The only time you're not as dumb as you look is when I close my eyes.",
-                f"{usermention} Imagine how many crises would have been averted if your parents bothered to use a condom.",
-                f"{usermention} You look like someone fed you after midnight.",
-                f"{usermention} Even Bob Ross would call you a mistake.",
-                f"{usermention} If I gave you a penny for your thoughts, I'd get change back.",
-                f'{usermention} Our parents told us we could be anything, and you chose "disappointment".',
-                f"{usermention} Your birth control of choice appears to be your personality.",
-                f"{usermention} If you’re going to be two-faced, at least make one of them cute.",
-                f"{usermention} If your IQ were just a little higher you could be an idiot.",
-                f"{usermention} You're not the dumbest person alive, but you better hope they're taking vitamins.",
-                f"{usermention} I'd love to help you out. Which way did you get in?",
-                f"{usermention} Light travels faster than sound, which is why you seemed bright until you spoke.",
-                f"{usermention} You're as bright as beige.",
-                f"{usermention} You look like something I drew with my left hand.",
-            ]
-        )
+        return choice(insults)
     elif lowered.startswith(PREFIX + "attr"):
         return get_attr(user_input[6:], usermention, channel)
     elif lowered.startswith(PREFIX + "fight"):
--- variables.py
+++ variables.py
@@ -10,3 +10,39 @@ attributes =  ['happy', 'bastard', 'mischievous', 'bashful', 'cute',
 attributes_nsfw = ['kinky', 'sub', 'dom', 'switch', 'tabboo', 
 'slut', 'spankable', 'daddy', 'boss', 'mistress', 'sir', 'master', 
 'tied', 'stimulated', 'orgasmic', 'sensual']
+
+insults = [
+                f"{usermention} The closest you'll come to a brainstorm is a light drizzle.",  # noqa: F821
+                f"{usermention} Honestly, I'm just impressed you could read this.",  # noqa: F821
+                f"{usermention} Your family tree didn't have enough branches.",  # noqa: F821
+                f"{usermention} Your face just made me agnostic.",  # noqa: F821
+                f"{usermention} I'm still deciding whether you're the weakest link or the missing link.",  # noqa: F821
+                f"{usermention} I have neither the time nor the crayons necessary to explain this to you.",  # noqa: F821
+                f"{usermention} You are an oxygen bandit.",  # noqa: F821
+                f"{usermention} I've had bowel movements more attractive than you.",  # noqa: F821
+                f'{usermention} You look like a "before" picture.',  # noqa: F821
+                f"{usermention} You could have been an extra in Chernobyl.",  # noqa: F821
+                f"{usermention} What doesn't kill you disappoints the rest of us.",  # noqa: F821
+                f"{usermention} Did you develop your personality in car a crash?",  # noqa: F821
+                f"{usermention} I smell smoke. Were you thinking too hard again?",  # noqa: F821
+                f"{usermention} Being bitter won't make you prettier.",  # noqa: F821
+                f"{usermention} Out of all the sperm to win the race...",  # noqa: F821
+                f"{usermention} You'll go far someday. And I hope you stay there.",  # noqa: F821
+                f"{usermention} Your gene pool needs more chlorine.",  # noqa: F821
+                f"{usermention} I will not have a battle of wits with an unarmed ,opponent.",  # noqa: F821
+                f"{usermention} It's impossible to underestimate you.",  # noqa: F821
+                f"{usermention} The only time you're not as dumb as you look is when I close my eyes.",  # noqa: F821
+                f"{usermention} Imagine how many crises would have been averted if your parents bothered to use a condom.",  # noqa: F821
+                f"{usermention} You look like someone fed you after midnight.",  # noqa: F821
+                f"{usermention} Even Bob Ross would call you a mistake.",  # noqa: F821
+                f"{usermention} If I gave you a penny for your thoughts, I'd get change back.",  # noqa: F821
+                f'{usermention} Our parents told us we could be anything, and you chose "disappointment".',  # noqa: F821
+                f"{usermention} Your birth control of choice appears to be your personality.",  # noqa: F821
+                f"{usermention} If you’re going to be two-faced, at least make one of them cute.",  # noqa: F821
+                f"{usermention} If your IQ were just a little higher you could be an idiot.",  # noqa: F821
+                f"{usermention} You're not the dumbest person alive, but you better hope they're taking vitamins.",  # noqa: F821
+                f"{usermention} I'd love to help you out. Which way did you get in?",  # noqa: F821
+                f"{usermention} Light travels faster than sound, which is why you seemed bright until you spoke.",  # noqa: F821
+                f"{usermention} You're as bright as beige.",  # noqa: F821
+                f"{usermention} You look like something I drew with my left hand.",  # noqa: F821
+            ]
